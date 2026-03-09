afe32f61a97a5d9d9a5099b20ee34f368d4eca9e
aaron GitHub <aaronse@outlook.com>
2026-03-08 17:26:56 -0700

    feat: implement line counting, enhance logging functionality and add build task configuration

--- Log.cs
+++ Log.cs
@@ -1,8 +1,176 @@
+using System.Diagnostics;
+using System.Text;
+
 namespace Lines;
 
 internal static class Log
 {
-    internal static void Info(string message) => Console.WriteLine(message);
+    private static string? _logFile;
+    private static bool _logTime = true;
+    private static bool _logMessageType = true;
+    private static bool _logRelativeTime;
+    private static readonly DateTime StartTime = DateTime.Now;
+    private static bool _verbose = true;
+    private static bool _consoleOutput = true;
+    private static bool _htmlOutput = true;
 
-    internal static void Error(string message) => Console.Error.WriteLine(message);
+    public static bool LogTime
+    {
+        get => _logTime;
+        set => _logTime = value;
+    }
+
+    public static bool LogMessageType
+    {
+        get => _logMessageType;
+        set => _logMessageType = value;
+    }
+
+    public static bool LogRelativeTime
+    {
+        get => _logRelativeTime;
+        set => _logRelativeTime = value;
+    }
+
+    public static bool Verbose
+    {
+        get => _verbose;
+        set => _verbose = value;
+    }
+
+    public static bool ConsoleOutput
+    {
+        get => _consoleOutput;
+        set => _consoleOutput = value;
+    }
+
+    public static bool HtmlOutput
+    {
+        get => _htmlOutput;
+        set => _htmlOutput = value;
+    }
+
+    public static void Error(string msg, params object[] args)
+    {
+        msg = string.Format(msg, args);
+        if (_consoleOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+        Write("[E]", msg);
+
+        if (_consoleOutput)
+        {
+            Console.ResetColor();
+        }
+    }
+
+    public static void Warn(string msg, params object[] args)
+    {
+        msg = string.Format(msg, args);
+        if (_consoleOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        Write("[W]", msg);
+
+        if (_consoleOutput)
+        {
+            Console.ResetColor();
+        }
+    }
+
+    public static void Info(string msg, params object[] args)
+    {
+        msg = string.Format(msg, args);
+        if (_consoleOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        Write("[I]", msg);
+
+        if (_consoleOutput)
+        {
+            Console.ResetColor();
+        }
+    }
+
+    public static void Debug(string msg, params object[] args)
+    {
+        if (!_verbose)
+        {
+            return;
+        }
+
+        msg = string.Format(msg, args);
+        if (_consoleOutput)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+        }
+
+        Write("[D]", msg);
+
+        if (_consoleOutput)
+        {
+            Console.ResetColor();
+        }
+    }
+
+    private static void Write(string msgType, string msg)
+    {
+        var builder = new StringBuilder();
+        if (LogMessageType)
+        {
+            builder.Append(msgType);
+            builder.Append(' ');
+        }
+
+        if (LogTime)
+        {
+            if (!LogRelativeTime)
+            {
+                builder.Append(DateTime.Now.ToString("HH:mm:ss.ff"));
+            }
+            else
+            {
+                var timeSpan = DateTime.Now.Subtract(StartTime);
+                builder.Append(string.Format("{0:D2}:{1:D2}:{2:D2}.{3:D3}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds, timeSpan.Milliseconds));
+            }
+
+            builder.Append(' ');
+        }
+
+        builder.Append(msg);
+        var line = builder.ToString();
+
+        if (_consoleOutput)
+        {
+            Console.WriteLine(_htmlOutput ? "<br/>" + line : line);
+        }
+
+        Trace.WriteLine(_htmlOutput ? "<br/>" + line : line);
+    }
+
+    public static void SetLogFile(string logFile)
+    {
+        _logFile = logFile;
+        Trace.Listeners.Clear();
+        if (string.IsNullOrEmpty(_logFile))
+        {
+            return;
+        }
+
+        Trace.Listeners.Add(new TextWriterTraceListener(_logFile));
+    }
+
+    public static void Close()
+    {
+        foreach (TraceListener listener in Trace.Listeners)
+        {
+            listener.Flush();
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,119 +1,370 @@
+using System.Diagnostics;
+using System.Text;
+
 namespace Lines;
 
-internal static class Program
+internal sealed class Program
 {
+    private const string DefaultIncludeFiles = "*.cs;*.java;*.c;*.cpp;*.h;*.hpp;*.sql;*.sqml;*.wxs;Descriptor.xml;*.asax;*.asmx;*.aspx;*.ascx;*.config;*.skin;*.js;*.css;*.xml;*.master;*.xsd;*.xslt";
+
+    private readonly string _usage =
+        "Line Count\r\n\r\n" +
+        "-d <dir>\r\n" +
+        "    Directory to search\r\n" +
+        "-i,-include <expr>\r\n" +
+        "    ; delimited file wildcard expression(s) of files to include,\r\n" +
+        "    defaults to... *.cs;*.java;*.py;*.c;*.cpp;*.h;*.hpp;*.sql;*.sqml;*.wxs;Descriptor.xml;*.asax;*.asmx;*.aspx;*.ascx;*.config;*.skin;*.js;*.css;*.xml;*.master;*.xsd;*.xslt\r\n" +
+        "-e,-exclude <expr>\r\n" +
+        "    ; delimited expression(s) used to exclude files.\r\n" +
+        "    NOTE: * wildcard characters not supported.\r\n" +
+        "-nt\r\n" +
+        "    No Totals are displayed\r\n" +
+        "-max <max byte size>\r\n" +
+        "    Parse files up to byte size specified\r\n" +
+        "-html\r\n" +
+        "    HTML output\r\n" +
+        "-sdc\r\n" +
+        "    Include SD changes\r\n" +
+        "-v\r\n" +
+        "    Verbose output\r\n" +
+        "-vt\r\n" +
+        "    Verbose output grouped by type\r\n" +
+        "-min-date <date string>\r\n" +
+        "    Exclude files older than specified date\r\n" +
+        "-max-date\r\n" +
+        "    Exclude files newer than specified date\r\n\r\n";
+
+    private readonly Dictionary<string, string> _args = new(StringComparer.OrdinalIgnoreCase);
+    private StringBuilder? _commandOutput;
+
     private static int Main(string[] args)
     {
-        if (args.Length > 0 && (args[0] == "-h" || args[0] == "--help"))
+        var program = new Program();
+        try
         {
-            Log.Info("Usage: lines [path]");
-            Log.Info("Counts lines in a file or recursively in a directory.");
+            program.Run(args);
             return 0;
         }
-
-        var targetPath = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
-        if (File.Exists(targetPath))
+        catch (Exception ex)
         {
-            var lines = CountFileLines(targetPath);
-            Log.Info($"{targetPath}: {lines}");
-            return 0;
-        }
-
-        if (!Directory.Exists(targetPath))
-        {
-            Log.Error($"Path not found: {targetPath}");
+            Log.Error("Unhandled Error, ex={0}", ex);
             return 1;
         }
+    }
 
-        long totalLines = 0;
-        long totalFiles = 0;
-
-        foreach (var filePath in EnumerateFilesSafe(targetPath))
+    private void ParseArgs(string[] args)
+    {
+        for (var index = 0; index < args.Length; index++)
         {
-            try
+            if (args[index].Length > 0 && args[index][0] == '-' && index + 1 < args.Length && (args[index + 1].Length == 0 || args[index + 1][0] != '-'))
             {
-                totalLines += CountFileLines(filePath);
-                totalFiles++;
+                _args[args[index++]] = args[index];
             }
-            catch (IOException)
+            else
             {
-                Log.Error($"Skipping unreadable file: {filePath}");
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Log.Error($"Skipping inaccessible file: {filePath}");
+                _args[args[index]] = string.Empty;
             }
         }
 
-        Log.Info($"Path: {targetPath}");
-        Log.Info($"Files: {totalFiles}");
-        Log.Info($"Lines: {totalLines}");
-
-        return 0;
-    }
-
-    private static IEnumerable<string> EnumerateFilesSafe(string rootPath)
-    {
-        var pendingDirectories = new Stack<string>();
-        pendingDirectories.Push(rootPath);
-
-        while (pendingDirectories.Count > 0)
+        if (_args.ContainsKey("-h") || _args.ContainsKey("-?") || _args.ContainsKey("/?"))
         {
-            var currentDirectory = pendingDirectories.Pop();
-
-            IEnumerable<string> files;
-            try
-            {
-                files = Directory.EnumerateFiles(currentDirectory);
-            }
-            catch (IOException)
-            {
-                Log.Error($"Skipping unreadable directory: {currentDirectory}");
-                continue;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Log.Error($"Skipping inaccessible directory: {currentDirectory}");
-                continue;
-            }
-
-            foreach (var filePath in files)
-            {
-                yield return filePath;
-            }
-
-            IEnumerable<string> childDirectories;
-            try
-            {
-                childDirectories = Directory.EnumerateDirectories(currentDirectory);
-            }
-            catch (IOException)
-            {
-                Log.Error($"Skipping unreadable directory: {currentDirectory}");
-                continue;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                Log.Error($"Skipping inaccessible directory: {currentDirectory}");
-                continue;
-            }
-
-            foreach (var childDirectory in childDirectories)
-            {
-                pendingDirectories.Push(childDirectory);
-            }
+            Log.Warn(_usage);
+            Environment.Exit(0);
         }
     }
 
-    private static long CountFileLines(string filePath)
+    private string GetArgValue(string name, string defaultValue)
     {
-        long lineCount = 0;
-        using var reader = new StreamReader(filePath);
-        while (reader.ReadLine() is not null)
+        return _args.TryGetValue(name, out var value) ? value : defaultValue;
+    }
+
+    private string GetArgValue(string[] names, string defaultValue)
+    {
+        foreach (var name in names)
         {
-            lineCount++;
+            if (_args.TryGetValue(name, out var value))
+            {
+                return value;
+            }
         }
 
-        return lineCount;
+        return defaultValue;
+    }
+
+    private DateTime GetArgValueDateTime(string name, DateTime defaultValue)
+    {
+        return _args.TryGetValue(name, out var value) ? DateTime.Parse(value) : defaultValue;
+    }
+
+    private void Run(string[] args)
+    {
+        Log.LogTime = false;
+        Log.LogMessageType = false;
+
+        ParseArgs(args);
+
+        var lineTerminator = "\n";
+        var searchDirectories = GetArgValue(new[] { "-d", "-dir" }, Environment.CurrentDirectory)
+            .Split(';', StringSplitOptions.RemoveEmptyEntries);
+        var includePatterns = GetArgValue(new[] { "-i", "-include" }, DefaultIncludeFiles)
+            .Split(';', StringSplitOptions.RemoveEmptyEntries);
+        var excludeFilters = GetArgValue(new[] { "-e", "-exclude" }, string.Empty)
+            .Split(';', StringSplitOptions.RemoveEmptyEntries);
+
+        var maxByteSize = long.Parse(GetArgValue("-max", "1048576"));
+        var verbose = _args.ContainsKey("-v");
+        var verboseByType = _args.ContainsKey("-vt");
+        Log.HtmlOutput = _args.ContainsKey("-html");
+        var includeSdChanges = _args.ContainsKey("-sdc");
+        var minDate = GetArgValueDateTime("-min-date", DateTime.MinValue);
+        var maxDate = GetArgValueDateTime("-max-date", DateTime.MaxValue);
+
+        if (!_args.ContainsKey("-nt") && verbose)
+        {
+            Log.Info("Searching from {0} for {1}", string.Join(";", searchDirectories), string.Join(";", includePatterns));
+        }
+
+        if (verbose)
+        {
+            Log.Info("{0,10} {1,10}    {2}", "Lines", "Size", "Path");
+        }
+
+        long totalIncludedLines = 0;
+        long totalIncludedFiles = 0;
+        long totalIncludedSize = 0;
+        long totalExcludedFiles = 0;
+        long totalExcludedLines = 0;
+        long totalExcludedSize = 0;
+
+        var seenFiles = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+        var typeSummaries = new SortedDictionary<string, FileTypeSummary>(StringComparer.OrdinalIgnoreCase);
+
+        foreach (var searchDirectory in searchDirectories)
+        {
+            foreach (var includePattern in includePatterns)
+            {
+                if (!Directory.Exists(searchDirectory))
+                {
+                    Log.Error("Directory not found: {0}", searchDirectory);
+                    continue;
+                }
+
+                foreach (var file in Directory.GetFiles(searchDirectory, includePattern, SearchOption.AllDirectories))
+                {
+                    if (!seenFiles.Add(file))
+                    {
+                        continue;
+                    }
+
+                    var excluded = false;
+                    foreach (var filter in excludeFilters)
+                    {
+                        var normalizedFilter = filter.Replace("*", string.Empty, StringComparison.Ordinal);
+                        if (file.Contains(normalizedFilter, StringComparison.OrdinalIgnoreCase))
+                        {
+                            excluded = true;
+                            break;
+                        }
+                    }
+
+                    var fileInfo = new FileInfo(file);
+                    var fileSize = fileInfo.Length;
+                    var countInFile = 0;
+                    var fileTooLarge = fileSize > maxByteSize && maxByteSize > 0;
+
+                    if (!fileTooLarge)
+                    {
+                        var fileContent = File.ReadAllText(file);
+                        var index = 0;
+                        while (index + 1 <= fileContent.Length)
+                        {
+                            index = fileContent.IndexOf(lineTerminator, index + 1, StringComparison.Ordinal);
+                            if (index == -1)
+                            {
+                                break;
+                            }
+
+                            countInFile++;
+                        }
+                    }
+
+                    if (minDate != DateTime.MinValue && fileInfo.LastWriteTime < minDate)
+                    {
+                        excluded = true;
+                    }
+
+                    if (maxDate != DateTime.MaxValue && fileInfo.LastWriteTime > maxDate)
+                    {
+                        excluded = true;
+                    }
+
+                    if (fileTooLarge)
+                    {
+                        if (verbose)
+                        {
+                            Log.Debug("-{0,9} {1,10}    {2}", "too big", fileSize, file);
+                        }
+
+                        totalExcludedSize += fileSize;
+                        totalExcludedFiles++;
+                    }
+                    else if (excluded)
+                    {
+                        if (verbose)
+                        {
+                            Log.Debug("-{0,9} {1,10}    {2}", countInFile, fileSize, file);
+                        }
+
+                        totalExcludedLines += countInFile;
+                        totalExcludedSize += fileSize;
+                        totalExcludedFiles++;
+                    }
+                    else
+                    {
+                        if (verbose)
+                        {
+                            Log.Info("{0,10} {1,10}    {2}", countInFile, fileSize, file);
+                        }
+
+                        totalIncludedLines += countInFile;
+                        totalIncludedSize += fileSize;
+                        totalIncludedFiles++;
+                        UpdateFileTypeSummary(typeSummaries, file, countInFile, fileSize);
+                    }
+                }
+            }
+
+            if (includeSdChanges)
+            {
+                GetSdChanges(searchDirectory);
+            }
+        }
+
+        if (verboseByType)
+        {
+            foreach (var key in typeSummaries.Keys)
+            {
+                var summary = typeSummaries[key];
+                Log.Info("{0,10} {1,10}    {2}", summary.CountInFile, summary.FileSize, key);
+                foreach (var file in summary.Files)
+                {
+                    Log.Info("{0,10} {1,10}    {2}", file.CountInFile, file.FileSize, file.FilePath);
+                }
+            }
+
+            Log.Info(string.Empty);
+            foreach (var key in typeSummaries.Keys)
+            {
+                var summary = typeSummaries[key];
+                Log.Info("{0,10} {1,10}    {2}", summary.CountInFile, summary.FileSize, key);
+            }
+        }
+
+        if (_args.ContainsKey("-nt"))
+        {
+            return;
+        }
+
+        Log.Info("{0} lines, {1}K, {2} files @ {3}", totalIncludedLines, totalIncludedSize / 1024L, totalIncludedFiles, Path.Combine(string.Join(";", searchDirectories), string.Join(";", includePatterns)));
+
+        if (verbose)
+        {
+            Log.Debug("Excluded file(s) {0}", totalExcludedFiles);
+            Log.Debug("{0,10} {1,10}    {2} exclude expr {3}", totalExcludedLines, totalExcludedSize, string.Join(";", searchDirectories), string.Join(";", excludeFilters));
+        }
+    }
+
+    private static void UpdateFileTypeSummary(SortedDictionary<string, FileTypeSummary> typeSummaries, string filePath, int countInFile, long fileSize)
+    {
+        var extension = Path.GetExtension(filePath);
+        if (!typeSummaries.ContainsKey(extension))
+        {
+            typeSummaries[extension] = new FileTypeSummary();
+        }
+
+        typeSummaries[extension].AddFile(filePath, countInFile, fileSize);
+    }
+
+    private void GetSdChanges(string baseDir)
+    {
+        var output = new StringBuilder();
+        ExecuteCommand("sd", $"changes -s submitted {baseDir}\\...", output);
+        Log.Info(output.ToString());
+    }
+
+    private int ExecuteCommand(string commandPath, string arguments, StringBuilder output)
+    {
+        using var process = new Process();
+        var startInfo = process.StartInfo;
+        startInfo.FileName = commandPath;
+        startInfo.Arguments = arguments;
+        startInfo.UseShellExecute = false;
+        startInfo.WorkingDirectory = Environment.CurrentDirectory;
+        startInfo.RedirectStandardOutput = true;
+        startInfo.RedirectStandardError = true;
+        startInfo.StandardOutputEncoding = Encoding.UTF8;
+        startInfo.StandardErrorEncoding = Encoding.UTF8;
+
+        _commandOutput = output;
+        process.OutputDataReceived += CommandOutputHandler;
+        process.ErrorDataReceived += CommandErrorHandler;
+
+        process.Start();
+        process.BeginOutputReadLine();
+        process.BeginErrorReadLine();
+        process.WaitForExit();
+        return process.ExitCode;
+    }
+
+    private void CommandOutputHandler(object sender, DataReceivedEventArgs args)
+    {
+        if (string.IsNullOrEmpty(args.Data))
+        {
+            return;
+        }
+
+        _commandOutput?.Append("[O]" + args.Data.Replace("\0", string.Empty, StringComparison.Ordinal) + "\n");
+    }
+
+    private void CommandErrorHandler(object sender, DataReceivedEventArgs args)
+    {
+        if (string.IsNullOrEmpty(args.Data))
+        {
+            return;
+        }
+
+        _commandOutput?.Append("[E]" + args.Data.Replace("\0", string.Empty, StringComparison.Ordinal) + "\n");
+    }
+
+    private sealed class FileTypeSummary
+    {
+        internal List<FileLineInfo> Files { get; } = [];
+
+        public int CountInFile { get; private set; }
+
+        public long FileSize { get; private set; }
+
+        internal void AddFile(string filePath, int countInFile, long fileSize)
+        {
+            CountInFile += countInFile;
+            FileSize += fileSize;
+            Files.Add(new FileLineInfo(filePath, countInFile, fileSize));
+        }
+    }
+
+    private sealed class FileLineInfo
+    {
+        public FileLineInfo(string filePath, int countInFile, long fileSize)
+        {
+            FilePath = filePath;
+            CountInFile = countInFile;
+            FileSize = fileSize;
+        }
+
+        public string FilePath { get; }
+
+        public int CountInFile { get; }
+
+        public long FileSize { get; }
     }
 }
